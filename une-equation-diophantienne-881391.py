d37b6e5ebc1ed1ad8bb0f3d6fed9bd468ac92588
bdaene <benoitdaene@gmail.com>
2022-08-10 11:12:03 +0200

    Add une-equation-diophantienne-881391.py.

---
+++ une-equation-diophantienne-881391.py
@@ -0,0 +1,27 @@
+
+# See https://www.ilemaths.net/sujet-une-equation-diophantienne-881391.html
+
+def solve(a):
+    p = 2*a
+    m = 12*a**p
+    b, c, bp, cp = 1, 2, 1, 2**p
+    while b < c:
+        if bp + m < cp:
+            b += 1
+            bp = b**p
+        else:
+            if bp + m == cp:
+                yield b, c
+            c += 1
+            cp = c**p
+
+
+def main():
+    for a in range(1, 1000):
+        print(a)
+        for b, c in solve(a):
+            print(f"{2*c}^{2*a} - {2*b}^{2*a} = 12({2*a})^{2*a}")
+
+
+if __name__ == "__main__":
+    main()
